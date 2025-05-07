4532c9ca0b9e1704acef4a0184971127534a5a8e
Whitelord11111 <White.lord11111@gmail.com>
2025-05-07 20:20:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config:
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
     FRAGMENT_API_KEY = os.getenv("FRAGMENT_API_KEY")
     CRYPTOBOT_TOKEN = os.getenv("CRYPTOBOT_TOKEN")
-    WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://yourdomain.com/webhook")
-    WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET", "your_secret_key")
-    STAR_PRICE_RUB = 1.6  # Цена одной звезды в рублях
+    WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
+    STAR_PRICE_RUB = 1.6
+    CRYPTO_API_URL = "https://pay.crypt.bot/api"
