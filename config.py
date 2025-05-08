cafb758d6ce101c598f53b32019a00c53f7fe26e
Whitelord11111 <White.lord11111@gmail.com>
2025-05-08 15:22:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-    FRAGMENT_API_KEY = os.getenv("FRAGMENT_API_KEY")
-    CRYPTOBOT_TOKEN = os.getenv("CRYPTOBOT_TOKEN")
-    WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
+    TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]  # Используем строгую проверку
+    FRAGMENT_API_KEY = os.environ.get("FRAGMENT_API_KEY", "")
+    CRYPTOBOT_TOKEN = os.environ["CRYPTOBOT_TOKEN"]
+    WEBHOOK_SECRET = os.environ["WEBHOOK_SECRET"]
     STAR_PRICE_RUB = 1.6
     CRYPTO_API_URL = "https://pay.crypt.bot/api"
